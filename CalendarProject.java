57b749288829d42e691ef50f8e6fbbd1b5f1f015
dohorcadrg <glorylegaspi@gmail.com>
2018-06-20 22:26:33 +1000

    Did somthing

--- CalendarProject.java
+++ CalendarProject.java
@@ -1,29 +1,45 @@
+/*********************
+*	Glory Legaspi,   *
+*		CS227		 *	
+*	SPRING 2018      *
+**********************/
+
 import java.util.Scanner;
 
 public class CalendarProject{
+
+//Variables Needed
 static Scanner scan = new Scanner(System.in);
 static int year;
 static int month;
 static String monthName;
 static boolean isItLeap;
 static int numberOfDays;
+static int SOLARDAYS = 365;
+static int LEAPDAYS = 366;
+static int yearCounting;
+static int leapYearCount;
+static int solarYearCount;
+static int daysCountBeforeYear;
+static int daysPassedInYear;
+static int weekStart;
+
 	
 	public static void main(String[] args) {
 		System.out.println("Enter a year (Example: 1892)");
 		year = scan.nextInt();
 		System.out.println("Enter a month in numeric value (Example: 1 for Janurary, 12 for December)");
 		month = scan.nextInt();
-		isItLeap = calcisItLeap(year);
-		numberOfDays = calcnumberOfDays(month);
 		System.out.println();
+		calcCalander();
 		System.out.println("This is this calendar for: ");
 		System.out.println();
 		System.out.println(monthName + " " + year);
-		calcDayOfWeek();
+		printCalander();
 		System.out.println();
 	}
 
-
+	//Check if year entered is a leap year
 	static boolean calcisItLeap(int y){
 		if(y % 4 == 0){
 			return true;
@@ -33,7 +49,7 @@ static int numberOfDays;
 		}
 	}
 
-
+	//Calculate what month is entered based on number value and the number of days in that month
 	static int calcnumberOfDays(int m){
 		int days;
 		switch (m){
@@ -97,14 +113,13 @@ static int numberOfDays;
 		return days;
 	}
 
+	//Calculate what day the month starts on
 	static void calcDayOfWeek(){
-		int SOLARDAYS = 365;
-		int LEAPDAYS = 366;
-		int yearCounting = year - 1;
-		int leapYearCount = yearCounting / 4;
-		int solarYearCount = yearCounting - leapYearCount;
-		int daysCountBeforeYear = (leapYearCount * LEAPDAYS) + (solarYearCount * SOLARDAYS);
-		int daysPassedInYear = 0;
+		yearCounting = year - 1;
+		leapYearCount = yearCounting / 4;
+		solarYearCount = yearCounting - leapYearCount;
+		daysCountBeforeYear = (leapYearCount * LEAPDAYS) + (solarYearCount * SOLARDAYS);
+		daysPassedInYear = 0;
 		for (int mnt = 0; mnt < month ; mnt++ ) {
 			switch (mnt){
 				case 1:
@@ -134,43 +149,67 @@ static int numberOfDays;
 				break;
 			}
 		}
-		int weekStart = (daysCountBeforeYear + daysPassedInYear) % 7;
+		weekStart = (daysCountBeforeYear + daysPassedInYear) % 7;
+	}
 
-		System.out.print("| Sun | Mon | Tue | Wen | Thr | Fri | Sat |");
-		System.out.println();
-		System.out.print("| ");
-		
+	//Shotcut function to calculate math needed for calander
+	static void calcCalander(){
+		isItLeap = calcisItLeap(year);
+		numberOfDays = calcnumberOfDays(month);
+		calcDayOfWeek();
+	}
+
+	static void printCalander(){
+		//BEGIN PRINTING OUT CALANDER
+		System.out.print("| Sun | Mon | Tue | Wed | Thr | Fri | Sat |");
+		makeNewLineAndMarkIt();
 		int printCount = weekStart + numberOfDays;
+		//PRINT EMPTY DAYS BEFORE THE DAY STARTS
 		if (weekStart > 0){
 			for (int i = 1; i <= weekStart ; i++ ) {
-				System.out.print("    | ");
+				makeEmptyLine();
 			}
 		}
-
+		//START PRINTING CALANDER DAYS
 		for (int d = 1; d <= numberOfDays ; d++ ) {
-
+			
 			while(d < 10){
+			//DAYS LESS THAN 10 HAVE + 0 BEFORE IT TO MAKE IT 01,02 , ETC..
 				System.out.print(" 0" + d + " | ");
-					if ((weekStart + d) % 7 == 0) {
-					System.out.println();
-					System.out.print("| ");
-					}
-					d++;
+				if ((weekStart + d) % 7 == 0) {
+					makeNewLineAndMarkIt();
+				}
+				d++; 
 			}
 
+			//WEHN DAY PRINTED OUT IS GREATER THAN 10, PRINT AS NORMAL
 			System.out.print(" " + d + " | ");
-
+			
+			//WHEN THE ROW OF DAYS IS OVER 7 AND WHEN IT IS NOT THE LAST LINE
 			if ((weekStart + d) % 7 == 0 && d <= numberOfDays-1) {
-				System.out.println();
-				System.out.print("| ");
+				makeNewLineAndMarkIt();
+				//Go To Next Line
 			}
 		}
+
+		//WHEN PRINTING THE CALANDER DAYS IS DONE AND THE LAST DAY DOES'NT END ON SATURDAY, PRINT OUT EMPTY LINES TO COMPLETE ROW
 			if (printCount % 7 != 0) {
 				for (int fc = printCount; fc % 7 != 0; fc++) {
-					System.out.print("    | ");
+					makeEmptyLine();
 				}
 			}
-		System.out.println(" ");
+		//Print next line
+		System.out.println();
+	}
+
+	//Shortcute functions for printing lines
+	static void makeNewLineAndMarkIt(){
+		System.out.println();
+		System.out.print("| ");
+	}
+
+	static void makeEmptyLine(){
+		System.out.print("    | ");
 	}
 
 }
